33dd8fe445eb6150dc47b9c0b4e9313ba169f586
Simon Cros <scros@student.42lyon.fr>
2020-11-26 11:10:12 +0100

    ft_memchr for overflows

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 17:57:55 by scros             #+#    #+#             */
-/*   Updated: 2020/11/25 12:31:23 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/11/26 11:03:11 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	char *cp;
+	unsigned char *cp;
 
-	cp = (char*)s;
+	cp = (unsigned char*)s;
+	c = (unsigned char)c;
 	while (n--)
 	{
 		if (*cp == c)
