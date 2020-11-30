88b90cd81d196e9070f51d22088c03e37628f28d
Simon Cros <scros@student.42lyon.fr>
2020-11-30 10:58:53 +0100

    Update memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 11:52:00 by scros             #+#    #+#             */
-/*   Updated: 2020/11/30 10:40:31 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/11/30 10:52:17 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,21 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	n[sizeof(long)];
+	long			lo;
 	unsigned char	*str;
 	size_t			i;
 
 	str = b;
+	lo = c;
 	i = 0;
-	while (i < sizeof(long))
-		n[i++] = c;
+	while (i++ < sizeof(long))
+		lo = lo << 8 | (unsigned char)c;
 	while (len)
 	{
 		if (len >= sizeof(long))
 		{
 			len -= sizeof(long);
-			*(long*)str = *(long*)n;
+			*(long*)str = lo;
 			str += sizeof(long);
 		}
 		else
