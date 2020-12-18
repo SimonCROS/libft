f4f2c610dd7e5a41fe128ee8242f2446bd80b221
Simon Cros <scros@student.42lyon.fr>
2020-12-18 15:14:34 +0100

    Initialize variable i in last two added functions

--- src/string/ft_strtolower.c
+++ src/string/ft_strtolower.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 14:44:09 by scros             #+#    #+#             */
-/*   Updated: 2020/12/18 14:47:14 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/12/18 15:13:58 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strtolower(char *str)
 {
 	int	i;
 
+	i = 0;
 	while (str[i])
 	{
 		str[i] = ft_tolower(str[i]);
--- src/string/ft_strtoupper.c
+++ src/string/ft_strtoupper.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 14:44:09 by scros             #+#    #+#             */
-/*   Updated: 2020/12/18 14:47:01 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/12/18 15:14:01 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strtoupper(char *str)
 {
 	int	i;
 
+	i = 0;
 	while (str[i])
 	{
 		str[i] = ft_toupper(str[i]);
