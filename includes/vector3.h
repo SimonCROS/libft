381e1e72b849d4cbb11b9a2137e966113b2d8db8
Simon Cros <scros@student.42lyon.fr>
2021-01-13 13:41:19 +0100

    Norm OK

--- includes/vector3.h
+++ includes/vector3.h
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 13:36:23 by scros             #+#    #+#             */
-/*   Updated: 2021/01/13 13:38:07 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/13 13:40:41 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,27 @@ typedef struct	s_vector3
 	double		z;
 }				t_vector3;
 
-double		ft_vector3_distance_squared(t_vector3 *v, double x, double y,
-			double z);
-double		ft_vector3_distance_squaredv(t_vector3 *v1, const t_vector3 *v2);
-double		ft_vector3_distance(t_vector3 *v, double x, double y, double z);
-t_vector3	*ft_vector3_init(t_vector3 *v, double x, double y, double z);
-t_vector3	*ft_vector3_viv(t_vector3 *v, double x, double y, double z);
-t_vector3	*ft_vector3_sub(t_vector3 *v, double x, double y, double z);
-t_vector3	*ft_vector3_mul(t_vector3 *v, double x, double y, double z);
-t_vector3	*ft_vector3_add(t_vector3 *v, double x, double y, double z);
-double		ft_vector3_distancev(t_vector3 *v1, const t_vector3 *v2);
-t_vector3	*ft_vector3_vivv(t_vector3 *v1, const t_vector3 *v2);
-t_vector3	*ft_vector3_subv(t_vector3 *v1, const t_vector3 *v2);
-t_vector3	*ft_vector3_mulv(t_vector3 *v1, const t_vector3 *v2);
-t_vector3	*ft_vector3_addv(t_vector3 *v1, const t_vector3 *v2);
-t_vector3	*ft_vector3_new(double x, double y, double z);
-double		ft_vector3_length_squared(const t_vector3 *v);
-t_vector3	*ft_vector3_vivd(t_vector3 *v, double a);
-t_vector3	*ft_vector3_muld(t_vector3 *v, double a);
-t_vector3	*ft_vector3_clone(const t_vector3 *v);
-double		ft_vector3_length(const t_vector3 *v);
-t_vector3	*ft_vector3_normalize(t_vector3 *v);
+double			ft_vector3_distance_squared(t_vector3 *v, double x, double y,
+				double z);
+double			ft_vector3_distance_squaredv(t_vector3 *v1,
+				const t_vector3 *v2);
+double			ft_vector3_distance(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_init(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_viv(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_sub(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_mul(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_add(t_vector3 *v, double x, double y, double z);
+double			ft_vector3_distancev(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_vivv(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_subv(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_mulv(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_addv(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_new(double x, double y, double z);
+double			ft_vector3_length_squared(const t_vector3 *v);
+t_vector3		*ft_vector3_vivd(t_vector3 *v, double a);
+t_vector3		*ft_vector3_muld(t_vector3 *v, double a);
+t_vector3		*ft_vector3_clone(const t_vector3 *v);
+double			ft_vector3_length(const t_vector3 *v);
+t_vector3		*ft_vector3_normalize(t_vector3 *v);
 
 #endif
