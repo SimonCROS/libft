e518705e789509abf07c1a0dd97c9bc47840760d
Simon Cros <scros@student.42lyon.fr>
2021-01-15 13:29:58 +0100

    Name of vector3 div functions patched

--- includes/vector3.h
+++ includes/vector3.h
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 13:36:23 by scros             #+#    #+#             */
-/*   Updated: 2021/01/14 13:18:39 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/15 13:28:48 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ double			ft_vector3_dot(const t_vector3 *v, double x, double y,
 double			ft_vector3_distance(t_vector3 *v, double x, double y, double z);
 t_vector3		*ft_vector3_cross(t_vector3 *v, double x, double y, double z);
 t_vector3		*ft_vector3_init(t_vector3 *v, double x, double y, double z);
-t_vector3		*ft_vector3_viv(t_vector3 *v, double x, double y, double z);
+t_vector3		*ft_vector3_div(t_vector3 *v, double x, double y, double z);
 t_vector3		*ft_vector3_sub(t_vector3 *v, double x, double y, double z);
 t_vector3		*ft_vector3_mul(t_vector3 *v, double x, double y, double z);
 t_vector3		*ft_vector3_add(t_vector3 *v, double x, double y, double z);
@@ -40,10 +40,10 @@ t_vector3		*ft_vector3_addv(t_vector3 *v1, const t_vector3 *v2);
 t_vector3		*ft_vector3_copy(t_vector3 *v1, const t_vector3 *v2);
 t_vector3		*ft_vector3_mulv(t_vector3 *v1, const t_vector3 *v2);
 t_vector3		*ft_vector3_subv(t_vector3 *v1, const t_vector3 *v2);
-t_vector3		*ft_vector3_vivv(t_vector3 *v1, const t_vector3 *v2);
+t_vector3		*ft_vector3_divv(t_vector3 *v1, const t_vector3 *v2);
 t_vector3		*ft_vector3_new(double x, double y, double z);
 double			ft_vector3_length_squared(const t_vector3 *v);
-t_vector3		*ft_vector3_vivd(t_vector3 *v, double a);
+t_vector3		*ft_vector3_divd(t_vector3 *v, double a);
 t_vector3		*ft_vector3_muld(t_vector3 *v, double a);
 t_vector3		*ft_vector3_clone(const t_vector3 *v);
 double			ft_vector3_length(const t_vector3 *v);
--- src/vector3/ft_vector3_div.c
+++ src/vector3/ft_vector3_div.c
@@ -6,13 +6,13 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 17:46:44 by scros             #+#    #+#             */
-/*   Updated: 2021/01/13 13:35:09 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/15 13:29:06 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector3.h"
 
-t_vector3	*ft_vector3_viv(t_vector3 *v, double x, double y, double z)
+t_vector3	*ft_vector3_div(t_vector3 *v, double x, double y, double z)
 {
 	v->x /= x;
 	v->y /= y;
@@ -20,12 +20,12 @@ t_vector3	*ft_vector3_viv(t_vector3 *v, double x, double y, double z)
 	return (v);
 }
 
-t_vector3	*ft_vector3_vivd(t_vector3 *v, double a)
+t_vector3	*ft_vector3_divd(t_vector3 *v, double a)
 {
-	return (ft_vector3_viv(v, a, a, a));
+	return (ft_vector3_div(v, a, a, a));
 }
 
-t_vector3	*ft_vector3_vivv(t_vector3 *v1, const t_vector3 *v2)
+t_vector3	*ft_vector3_divv(t_vector3 *v1, const t_vector3 *v2)
 {
-	return (ft_vector3_viv(v1, v2->x, v2->y, v2->z));
+	return (ft_vector3_div(v1, v2->x, v2->y, v2->z));
 }
