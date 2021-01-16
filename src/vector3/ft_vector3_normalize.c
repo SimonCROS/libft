77eb56029a4964f6ae8a588b8695d786b031bfa7
Simon Cros <scros@student.42lyon.fr>
2021-01-16 14:11:29 +0100

    Patch normalize for 0 inputs

--- src/vector3/ft_vector3_normalize.c
+++ src/vector3/ft_vector3_normalize.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 16:58:02 by scros             #+#    #+#             */
-/*   Updated: 2021/01/16 13:33:12 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/16 14:10:59 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_vector3	ft_vector3_normalize(const t_vector3 *v)
 	float	length;
 
 	length = ft_vector3_length(v);
-	if (ft_abs(length) < 0)
+	if (length == 0)
 		return (ft_vector3_news(0, 0, 0));
 	return (ft_vector3_divd(v, length));
 }
