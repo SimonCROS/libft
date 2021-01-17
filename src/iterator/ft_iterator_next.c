be6e5a57365258d457557f54ab05ca43677f0d65
Simon Cros <scros@student.42lyon.fr>
2021-01-17 10:39:56 +0100

    Patch iterator

--- src/iterator/ft_iterator_next.c
+++ src/iterator/ft_iterator_next.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 17:02:11 by scros             #+#    #+#             */
-/*   Updated: 2021/01/16 17:06:28 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/17 10:39:29 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	*ft_iterator_next(t_iterator *iterator)
 {
 	if (ft_iterator_has_next(iterator))
 		iterator->current = iterator->current->next;
-	return (iterator->current);
+	return (iterator->current->value);
 }
