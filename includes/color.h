ff64c78eb5243eabf36be0b106424b57111cd5d9
Simon Cros <scros@student.42lyon.fr>
2021-01-18 10:10:25 +0100

    Update some functions on colors

--- includes/color.h
+++ includes/color.h
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 15:54:41 by scros             #+#    #+#             */
-/*   Updated: 2021/01/17 15:59:56 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/18 10:09:37 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,10 @@ t_color				ft_color_from_hsla(unsigned short h, unsigned char s,
 	unsigned char l, unsigned char a);
 t_color				ft_color_from_rgba(unsigned char r, unsigned char g,
 	unsigned char b, unsigned char a);
-t_color				ft_color_sub(const t_color v, unsigned char r, unsigned char
-	g, unsigned char b);
-t_color				ft_color_mul(const t_color v, unsigned char r, unsigned char
-	g, unsigned char b);
-t_color				ft_color_div(const t_color v, unsigned char r, unsigned char
-	g, unsigned char b);
-t_color				ft_color_add(const t_color v, unsigned char r, unsigned char
-	g, unsigned char b);
+t_color				ft_color_sub(const t_color v, float r, float g, float b);
+t_color				ft_color_mul(const t_color v, float r, float g, float b);
+t_color				ft_color_div(const t_color v, float r, float g, float b);
+t_color				ft_color_add(const t_color v, float r, float g, float b);
 t_color				ft_color_from_hsl(unsigned short h, unsigned char s,
 	unsigned char l);
 t_color				ft_color_from_rgb(unsigned char r, unsigned char g, unsigned
@@ -41,10 +37,10 @@ t_color				ft_color_subv(const t_color v1, const t_color v2);
 t_color				ft_color_mulv(const t_color v1, const t_color v2);
 t_color				ft_color_divv(const t_color v1, const t_color v2);
 t_color				ft_color_addv(const t_color v1, const t_color v2);
-t_color				ft_color_subd(const t_color v, unsigned char a);
-t_color				ft_color_muld(const t_color v, unsigned char a);
-t_color				ft_color_divd(const t_color v, unsigned char a);
-t_color				ft_color_addd(const t_color v, unsigned char a);
+t_color				ft_color_subd(const t_color v, float a);
+t_color				ft_color_muld(const t_color v, float a);
+t_color				ft_color_divd(const t_color v, float a);
+t_color				ft_color_addd(const t_color v, float a);
 t_color				ft_color_from_hexa(unsigned int hex);
 t_color				ft_color_from_hex(unsigned int hex);
 unsigned int		ft_color_to_hexa(const t_color c);
--- src/color/ft_color_div.c
+++ src/color/ft_color_div.c
@@ -6,25 +6,24 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 17:46:44 by scros             #+#    #+#             */
-/*   Updated: 2021/01/17 15:56:56 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/18 10:08:22 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftmath.h"
 #include "color.h"
 
-t_color	ft_color_div(const t_color v, unsigned char r, unsigned char g, unsigned
-	char b)
+t_color	ft_color_div(const t_color v, float r, float g, float b)
 {
 	t_color vec;
 
-	vec.r = ft_max(ft_min(v.r / r, 255), 0);
-	vec.g = ft_max(ft_min(v.g / g, 255), 0);
-	vec.b = ft_max(ft_min(v.b / b, 255), 0);
+	vec.r = ft_fmax(ft_fmin(v.r / r, 255), 0);
+	vec.g = ft_fmax(ft_fmin(v.g / g, 255), 0);
+	vec.b = ft_fmax(ft_fmin(v.b / b, 255), 0);
 	return (vec);
 }
 
-t_color	ft_color_divd(const t_color v, unsigned char a)
+t_color	ft_color_divd(const t_color v, float a)
 {
 	return (ft_color_div(v, a, a, a));
 }
