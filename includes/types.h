83228cc79cb452c4da4be161036883497ae6bc74
Simon Cros <scros@student.42lyon.fr>
2021-01-26 15:54:59 +0100

    Add suppliers types

--- includes/types.h
+++ includes/types.h
@@ -6,16 +6,22 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:06:43 by scros             #+#    #+#             */
-/*   Updated: 2021/01/26 14:46:13 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/26 15:54:28 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
+# define bool int
+# define true 1
+# define false 0
+
 typedef void		*(*t_function)(void *);
 typedef void		*(*t_bifunction)(void *, void *);
 typedef void		(*t_consumer)(void *);
 typedef void		(*t_biconsumer)(void *, void *);
+typedef bool		(*t_predicate)(void *);
+typedef bool		(*t_bipredicate)(void *, void *);
 
 #endif
