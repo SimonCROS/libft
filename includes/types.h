40a7ffa8034391a41aed59029c25bde24cd13ea2
Simon Cros <scros@student.42lyon.fr>
2021-01-26 16:01:57 +0100

    Fix norminette

--- includes/types.h
+++ includes/types.h
@@ -6,15 +6,15 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:06:43 by scros             #+#    #+#             */
-/*   Updated: 2021/01/26 16:01:05 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/26 16:01:46 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
-# define true 1
-# define false 0
+# define TRUE 1
+# define FALSE 0
 
 typedef void		*(*t_function)(void *);
 typedef void		*(*t_bifunction)(void *, void *);
