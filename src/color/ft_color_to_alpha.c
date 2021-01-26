00fe4cf58b6caaa17fb139ee3c6b3fe3e27926ba
Simon Cros <scros@student.42lyon.fr>
2021-01-26 13:42:09 +0100

    Patch something on color

--- src/color/ft_color_to_alpha.c
+++ src/color/ft_color_to_alpha.c
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 14:22:20 by scros             #+#    #+#             */
-/*   Updated: 2021/01/26 13:33:59 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/26 13:41:46 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 unsigned int	ft_color_to_hexa(const t_color c)
 {
-	return (255 - (c.a << 24) | c.r << 16 | c.g << 8 | c.b);
+	return ((255 - c.a) << 24 | c.r << 16 | c.g << 8 | c.b);
 }
