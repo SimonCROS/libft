31bb7ce8210008ceae4eeea02e2fdbc9d6838f57
Simon Cros <scros@student.42lyon.fr>
2021-01-26 16:01:13 +0100

    Remove bool type

--- includes/types.h
+++ includes/types.h
@@ -6,14 +6,13 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:06:43 by scros             #+#    #+#             */
-/*   Updated: 2021/01/26 15:54:28 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/01/26 16:01:05 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
-# define bool int
 # define true 1
 # define false 0
 
@@ -21,7 +20,7 @@ typedef void		*(*t_function)(void *);
 typedef void		*(*t_bifunction)(void *, void *);
 typedef void		(*t_consumer)(void *);
 typedef void		(*t_biconsumer)(void *, void *);
-typedef bool		(*t_predicate)(void *);
-typedef bool		(*t_bipredicate)(void *, void *);
+typedef int			(*t_predicate)(void *);
+typedef int			(*t_bipredicate)(void *, void *);
 
 #endif
