ff83ffbcf930e85f0450e19d75e4e9411b384dc0
Simon Cros <scros@student.42lyon.fr>
2021-02-12 15:15:10 +0100

    Patch

--- src/color/color_to.c
+++ src/color/color_to.c
@@ -6,11 +6,12 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 14:22:20 by scros             #+#    #+#             */
-/*   Updated: 2021/02/12 15:11:25 by scros            ###   ########lyon.fr   */
+/*   Updated: 2021/02/12 15:14:57 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "color.h"
+#include <math.h>
 
 unsigned int	ft_color_to_hexa(const t_color c)
 {
@@ -19,10 +20,10 @@ unsigned int	ft_color_to_hexa(const t_color c)
 	unsigned char b;
 	unsigned char a;
 
-	r = c.r * 255;
-	g = c.g * 255;
-	b = c.b * 255;
-	a = 255 - (c.a * 255);
+	r = fminf(c.r, 1) * 255;
+	g = fminf(c.g, 1) * 255;
+	b = fminf(c.b, 1) * 255;
+	a = 255 - (fminf(c.a, 1) * 255);
 	return (a << 24 | r << 16 | g << 8 | b);
 }
 
