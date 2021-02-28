4075bb94b0981bc4515a1566b8465049108988bc
Simon Cros <simon.dc.cros@gmail.com>
2021-02-28 22:14:59 +0100

    atof_full and atoi_full added and commented

--- src/convert/ft_atof.c
+++ src/convert/ft_atof.c
@@ -2,7 +2,50 @@
  * @brief Convert a string to a float
  * 
  * @param str the string to convert
- * @return float the final float
+ * @param result the final float, same result as ft_atof
+ * @return 1 if the result matches ('^-?[0-9]*(\.[0-9]+)?$'), 0 else
+ */
+
+int	ft_atof_full(char *str, float *result)
+{
+	int		dot;
+	float	ret;
+	float	mul;
+
+	ret = 0;
+	mul = 1;
+	dot = 0;
+	if (*str == '-' && str++)
+		mul = -1;
+	while ((*str >= '0' && *str <= '9') || (*str == '.' && !dot))
+	{
+		if (dot)
+			dot++;
+		if (*str == '.')
+			dot = 1;
+		else
+		{
+			if (dot)
+				mul /= 10.0f;
+			ret = ret * 10.0f + (float)(*str - '0');
+		}
+		str++;
+	}
+	*result = ret * mul;
+	return (!(*str) && dot != 1);
+}
+
+/**
+ * @brief Convert a string to a float
+ * 
+ * Exemple
+ * `ft_atof("42") == 42`
+ * `ft_atof("") == 0`
+ * `ft_atof("1234.5 1") == 1234.5`
+ * `ft_atof(".21") == 0.21`
+ * 
+ * @param str the string to convert
+ * @return the final float
  */
 
 float	ft_atof(char *str)
@@ -32,32 +75,4 @@ float	ft_atof(char *str)
 		str++;
 	}
 	return (ret * mul);
-}
-
-/**
- * @brief Check is a string is at float format ('^[0-9]*(\.[0-9])?$')
- * 
- * @param str the string to convert
- * @param result the float
- * @return if the string is a float
- */
-
-int	is_float(char *str)
-{
-	int	dot;
-
-	dot = 0;
-	if (!(*str))
-		return (0);
-	if (*str == '-')
-		str++;
-	while ((*str >= '0' && *str <= '9') || (*str == '.' && !dot))
-	{
-		if (dot)
-			dot++;
-		if (*str == '.')
-			dot = 1;
-		str++;
-	}
-	return (dot != 1 && !(*str));
 }
--- src/convert/ft_atoi.c
+++ src/convert/ft_atoi.c
@@ -1,20 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/24 11:26:15 by scros             #+#    #+#             */
-/*   Updated: 2020/12/13 15:05:06 by scros            ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @brief Convert a string to a int
+ * 
+ * @param str the string to convert
+ * @param result the final int, same result as ft_atof
+ * @return 1 if the result matches ('^-?[0-9]+$'), 0 else
+ */
 
-#include "libft.h"
-
-int	ft_atoi(const char *str)
+int	ft_atoi_full(char *str, int *result)
 {
-	int	len;
+	int	ret;
+	int	mul;
 
-	return (ft_atoi_len(str, &len));
+	ret = 0;
+	mul = 1;
+	if (*str == '-')
+	{
+		str++;
+		mul = -1;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		ret = ret * 10 + (*str - '0');
+		str++;
+	}
+	*result = ret * mul;
+	return (!(*str));
+}
+
+/**
+ * @brief Convert a string to a int
+ * 
+ * Exemple
+ * `ft_atoi("42") == 42`
+ * `ft_atoi("") == 0`
+ * `ft_atoi("1234.5 1") == 1234.5`
+ * `ft_atoi(".21") == 0.21`
+ * 
+ * @param str the string to convert
+ * @return the final int
+ */
+
+int	ft_atoi(char *str)
+{
+	int	ret;
+	int	mul;
+
+	ret = 0;
+	mul = 1;
+	if (*str == '-')
+	{
+		str++;
+		mul = -1;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		ret = ret * 10 + (*str - '0');
+		str++;
+	}
+	return (ret * mul);
 }
