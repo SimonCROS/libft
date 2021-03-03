f7e05959ffb45587b22eb010180c8170668163df
Simon Cros <simon.dc.cros@gmail.com>
2021-03-03 21:45:44 +0100

    Remove unused variable

--- src/bitmap/bmp_set_pixel.c
+++ src/bitmap/bmp_set_pixel.c
@@ -3,11 +3,9 @@
 
 void	bmp_set_pixel(t_bitmap *bitmap, int x, int y, t_color color)
 {
-	int		pos;
-	short	bytes_per_pixel;
+	int	pos;
 
 	pos = (bitmap->header.infoheader.height - y) * x;
-	bytes_per_pixel = bitmap->header.infoheader.bitsperpixel / 8;
 	bitmap->body[pos] = (unsigned char)fminf(color.r, 1) * 255;
 	bitmap->body[pos + 1] = (unsigned char)fminf(color.r, 1) * 255;
 	bitmap->body[pos + 2] = (unsigned char)fminf(color.r, 1) * 255;
