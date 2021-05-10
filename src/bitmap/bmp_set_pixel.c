d114475a4a982b1ea8458405f0d6799b70239a9d
Simon Cros <simon.dc.cros@gmail.com>
2021-05-10 20:05:19 +0200

    Add bmp_get_pixel

--- src/bitmap/bmp_set_pixel.c
+++ src/bitmap/bmp_set_pixel.c
@@ -13,3 +13,14 @@ void	bmp_set_pixel(t_bitmap *bitmap, int x, int y, t_color color)
 	bitmap->body[pos + 2] = (char)(fminf(color.r, 1) * 255);
 	bitmap->body[pos + 3] = 0x0;
 }
+
+t_color	bmp_get_pixel(t_bitmap *bitmap, int x, int y)
+{
+	t_bmpinfoheader	*inf;
+	int				pos;
+
+	inf = &(bitmap->header.infos);
+	pos = ((inf->height - y - 1) * inf->width + x) * (inf->bitsperpixel / 8);
+	return (color_new(bitmap->body[pos + 2], bitmap->body[pos + 1],
+			bitmap->body[pos]));
+}
