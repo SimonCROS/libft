f96d7ea6ce55778b029195c9c6129a627a3ca5f3
SimonCROS <simon.dc.cros@gmail.com>
2021-05-15 22:59:02 +0200

    Patch color_from_hexa

--- src/color/color_from.c
+++ src/color/color_from.c
@@ -5,10 +5,10 @@ t_color	color_newa(unsigned char r, unsigned char g, unsigned char b
 {
 	t_color	color;
 
-	color.r = r / (float) 255;
-	color.g = g / (float) 255;
-	color.b = b / (float) 255;
-	color.a = a / (float) 255;
+	color.r = r / (float)255;
+	color.g = g / (float)255;
+	color.b = b / (float)255;
+	color.a = a / (float)255;
 	return (color);
 }
 
@@ -16,10 +16,10 @@ t_color	color_from_hexa(unsigned int hex)
 {
 	t_color	color;
 
-	color.a = (255 - (hex >> 24)) / (float) 255;
-	color.r = (hex >> 16) / (float) 255;
-	color.g = (hex >> 8) / (float) 255;
-	color.b = (hex) / (float) 255;
+	color.a = (255 - (unsigned char)(hex >> 24)) / (float)255;
+	color.r = (unsigned char)(hex >> 16) / (float)255;
+	color.g = (unsigned char)(hex >> 8) / (float)255;
+	color.b = (unsigned char)(hex) / (float)255;
 	return (color);
 }
 
