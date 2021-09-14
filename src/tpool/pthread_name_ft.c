9c9ddf90b960fb37b19b5a8af3ba88c4b3d473cf
Simon Cros <simon.dc.cros@gmail.com>
2021-09-14 20:23:24 +0200

    Norm pthread_name_ft

--- src/tpool/pthread_name_ft.c
+++ src/tpool/pthread_name_ft.c
@@ -2,16 +2,19 @@
 #include <pthread.h>
 
 #if defined __APPLE__
+
 int	pthread_setname_ft(char *name)
 {
 	return (pthread_setname_np(name));
 }
 
 #elif defined __linux__
+
 int	pthread_setname_ft(char *name)
 {
 	return (pthread_setname_np(pthread_self(), name));
 }
+
 #endif
 
 int	pthread_getname_ft(char *name, size_t buffer_len)
